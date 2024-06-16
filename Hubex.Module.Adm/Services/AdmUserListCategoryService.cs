bcf474642ce5e0f538116ce73e741fe1bc1c2220
george <georgevolkov@gmail.com>
2024-06-16 17:19:16 +0600

    Refactoring GetUserListCategoriesAsync

--- Hubex.Module.Adm/Services/AdmUserListCategoryService.cs
+++ Hubex.Module.Adm/Services/AdmUserListCategoryService.cs
@@ -7,40 +7,38 @@ namespace Hubex.Module.Adm.Services;
 
 public class AdmUserListCategoryService : IAdmUserListCategoryService
 {
-    private readonly AdmDbContext _context;
+   private readonly AdmDbContext _context;
 
-    public AdmUserListCategoryService(AdmDbContext context)
-    {
-        _context = context;
-    }
+   public AdmUserListCategoryService(AdmDbContext context)
+   {
+      _context = context;
+   }
 
-    public async Task<IEnumerable<UserTaskListCategory>> GetUserListCategoriesAsync(int tenantId)
-    {
-       var userRoles = await _context.UserRoles
-          .Where(ur => ur.TenantId == tenantId && ur.Deleted == null)
-          .ToListAsync();
+   public async Task<IEnumerable<UserTaskListCategory>> GetUserListCategoriesAsync(int tenantId)
+   {
+      var users = await _context.Users
+         .Include(u => u.UserRoles)
+         .ThenInclude(r => r.RolePermissions)
+         .ToListAsync();
 
-       var rolePermissions = await _context.RolePermissions
-          .Where(rpe => rpe.TenantId == tenantId && rpe.Deleted == null)
-          .ToListAsync();
+      var listCategories = await _context.ListCategories.ToListAsync();
 
-       var users = await _context.Users
-          .Include(u => u.UserRoles)
-          .ToListAsync();
+      var result = users.SelectMany(u => listCategories
+         .Where(lc =>
+            u.UserRoles.Any(ur =>
+               ur.TenantId == tenantId &&
+               ur.Deleted == null &&
+               ur.RolePermissions.Any(rpe =>
+                  rpe.TenantId == tenantId &&
+                  rpe.Deleted == null &&
+                  rpe.PermissionExtId == lc.PermissionExtId)) ||
+            lc.PermissionExtId == null)
+         .Select(lc => new UserTaskListCategory
+         {
+            UserId = u.Id,
+            TaskListCategoryId = lc.Id
+         })).ToList();
 
-       var listCategories = await _context.ListCategories.ToListAsync();
-
-       var result = users.SelectMany(u => listCategories.Where(lc =>
-             userRoles.Any(ur => ur.UserId == u.Id &&
-                                 rolePermissions.Any(rpe => rpe.RoleId == ur.RoleId &&
-                                                            rpe.PermissionExtId == lc.PermissionExtId)) ||
-             lc.PermissionExtId == null)
-          .Select(lc => new UserTaskListCategory
-          {
-             UserId = u.Id,
-             TaskListCategoryId = lc.Id
-          })).ToList();
-
-       return result;
-    }
+      return result;
+   }
 }
